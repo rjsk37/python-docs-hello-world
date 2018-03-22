fe069a20c4adeba264bc8c2c56dcd185b4bced79
rjsk37 <rjsk37@users.noreply.github.com>
2018-03-22 11:13:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World! -Rajasekaran v0.1'
+  return 'Hello, World! -Rajasekaran v0.2'
 
 if __name__ == '__main__':
   app.run()
